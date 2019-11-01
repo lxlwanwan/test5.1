826d9759bc945fb0c7c6b33b0c3597b70d48bb37
liuxiao <liuxiao@qq.com>
2019-11-01 17:57:08 +0800

    测试1

--- application/index/controller/Text.php
+++ application/index/controller/Text.php
@@ -9,7 +9,7 @@ class Text extends Common {
     public function index(){
         $user= User::get(6);
         $a=new User();
-        $user=$a->comments()->where('id',6)->select();
+        $user=$a->comments(6)->select();
         dump($user);
     }
 
--- application/index/model/User.php
+++ application/index/model/User.php
@@ -14,9 +14,12 @@ class User extends Model{
     /**
      * @return \think\model\relation\HasMany 1对多关联
      */
-    public  function comments(){
-
-        return $this->hasMany('post','uid','id');
+    public  function comments($id=[]){
+        $where=[];
+        if($id) {
+            $where[] = ['id', 'eq', $id];
+        }
+        return $this->hasMany('post','uid','id')->where($where);
     }
 
 
