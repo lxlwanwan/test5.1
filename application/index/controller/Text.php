4b8de827c9112fd4b15fc3c77dd50c9494950b8f
liuxiao <liuxiao@qq.com>
2019-11-01 14:40:28 +0800

    测试

--- application/index/controller/Text.php
+++ application/index/controller/Text.php
@@ -6,18 +6,42 @@ use think\Controller;
 
 class Text extends Controller{
 
+    protected $beforeActionList=[
+
+        'comment'=>['except'=>'info,posts'],
+        'hello' =>['only'=>'test,index']
+
+    ];
+
+
+    public function comment(){
+
+        return 'comment!<br/>';
+    }
+
+    public function info(){
+
+        return 'info!<br/>';
+    }
+
+    public function posts(){
+
+        return 'posts!<br/>';
+    }
+
+    public function hello(){
+
+        return 'hello!<br/>';
+    }
 
     public function test(){
 
-       return 'hello!';
-
+       return 'test!<br/>';
     }
-
 
     public function index(){
 
-        return 'index!';
-
+        return 'index!<br/>';
     }
 
 
