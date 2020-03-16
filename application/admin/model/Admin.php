efa1ca0e52b5fe6b68d1694d9b892d50d41d0fc2
liuxiao <liuxiao@qq.com>
2020-03-16 14:55:51 +0800

    后台

--- application/admin/model/Admin.php
+++ application/admin/model/Admin.php
@@ -18,21 +18,21 @@ class Admin extends Model{
      */
     public static function log_in($input=[]){
         if(isset($input['name'])==false || isset($input['password'])==false){
-            return ['err'=>201,'msg'=>'参数不正确'];
+            return json(['err'=>201,'msg'=>'参数不正确']);
         }
         $admin = self::where('name',$input['name'])->find()->toArray();
         if(empty($admin)){
-            return ['err'=>201,'msg'=>'管理员不存在'];
+            return json(['err'=>201,'msg'=>'管理员不存在']);
         }
         if($admin['state'] ==1){
-            return ['err'=>201,'msg'=>'账号禁止登陆'];
+            return json(['err'=>201,'msg'=>'账号禁止登陆']);
         }
         if(!password_verify($input['password'],$admin['password'])){
-            return ['err'=>201,'msg'=>'密码错误'];
+            return json(['err'=>201,'msg'=>'密码错误']);
         }
         Cookie::set('admin',$admin,7200);
         LogList::add_log($admin,'管理员登陆');
-        return ['err'=>200,'msg'=>'登陆成功'];
+        return json(['err'=>200,'msg'=>'登陆成功']);
     }
 
 
