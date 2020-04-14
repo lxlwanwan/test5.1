ef3f01bf5a20fde257293bbd6d76c7c3f99540ca
liuxiao <liuxiao@qq.com>
2020-04-14 19:42:49 +0800

    添加富文本

--- application/index/controller/Text.php
+++ application/index/controller/Text.php
@@ -2,12 +2,18 @@
 namespace app\index\controller;
 
 
+use app\admin\model\Admin;
 use app\index\model\User;
+use think\cache\driver\Redis;
 
 class Text extends Common {
 
     public function index(){
-        phpinfo();
+        $key ='test_list';
+        $admin = Admin::where('id',1)->find()->toArray();
+        $redis = new Redis();
+        $a = $redis->hSet($key,$admin['id'],$admin);
+        dump($a);
 //        $user= User::get(6);
 //        $a=new User();
 //        $user=$user->onehas()->find();
